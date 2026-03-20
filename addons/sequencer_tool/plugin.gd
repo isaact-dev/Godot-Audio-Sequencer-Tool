7e0db4ae0b82307a37e0b3b29536a2668453b59c
Core-B <isaactaes7@gmail.com>
2026-03-20 20:04:27 +0100

    Build initial dock layout
    Build initial dock layout

--- addons/sequencer_tool/plugin.gd
+++ addons/sequencer_tool/plugin.gd
@@ -1,22 +1,31 @@
 @tool
 extends EditorPlugin
-
+var dock
 
 func _enable_plugin():
-	# Add autoloads here.
-	pass
+	print("Godot Audio Sequencer Tool enabled")
+
 
 
 func _disable_plugin():
-	# Remove autoloads here.
-	pass
+	print("Godot Audio Sequencer Tool disabled")
+
 
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	var dock_scene = preload("res://addons/sequencer_tool/ui/sequencer_dock.tscn").instantiate()
+	# Create the dock and add the loaded scene to it.
+	dock = EditorDock.new()
+	dock.add_child(dock_scene)
+	# Add dock specifications
+	dock.title = "Audio Sequencer"
+	dock.default_slot = EditorDock.DOCK_SLOT_BOTTOM
+	dock.available_layouts = EditorDock.DOCK_LAYOUT_HORIZONTAL
+	
+	add_dock(dock)
+	
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+	remove_dock(dock)
+	dock.free()
---
+++ addons/sequencer_tool/ui/sequencer_dock.gd
@@ -0,0 +1,21 @@
+extends VBoxContainer
+
+
+func _on_button_new_pressed():
+	print('new')
+
+
+func _on_button_open_pressed():
+	print('open')
+
+
+func _on_button_save_pressed():
+	print('save')
+
+
+func _on_button_play_pressed():
+	print('play')
+
+
+func _on_button_pause_pressed():
+	print('pause')
